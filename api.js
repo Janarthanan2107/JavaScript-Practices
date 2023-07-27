421d9b092fbee647ae9f3c8d366564c20c5694b0
Janarthanan2107 <131163725+Janarthanan2107@users.noreply.github.com>
2023-07-27 09:48:29 +0530

    api practice

---
+++ api.js
@@ -0,0 +1,81 @@
+// // sync , async ,fetch
+
+// // error handling , await
+// // try,catch,finally
+
+// const url = `https://fakestoreapi.com/products`;
+// // const url = `https://randomuser.me/api/`;
+
+// // fetch and then are a async function
+// try {
+//     fetch(url)
+//         // fetch() will give a promise for that we process that using then()
+//         .then((res) => res.json())
+//         //this first then() or res.json() are a promise only
+//         .then((data) => product(data))
+//     //this second then will provide the data that the response provide, here we store the value in product functions argument
+// } catch (err) {
+//     //catch block will get the error from the try and it perform the action
+//     console.log(err)
+// } finally {
+//     // finally, if error comes or not whatever happens will execute the action wrote inside the block
+//     console.log("hello")
+// }
+
+// const product = (data) => {
+//     console.log(data)
+// }
+
+// <---------------------------Example for api call--------------------------------->
+
+// const btn = document.getElementById("btn")
+// const inputEl = document.getElementById("input")
+// const outputEl = document.getElementById("output")
+
+// btn.addEventListener("click", () => {
+//     const inputText = inputEl.value
+//     // outputEl.innerText = inputText
+
+//     let url = `https://api.funtranslations.com/translate/pig-latin.json?text=`;
+//     url = url + inputText
+
+//     try {
+//         fetch(url)
+//             .then((res) => res.json())
+//             .then((data) => {
+//                 outputEl.innerText = data.contents.translated
+//             })
+//     } catch (err) {
+//         console.log(err)
+//     }
+// })
+
+// async awaits
+
+const url = `https://fakestoreapi.com/products`;
+
+// try {
+//     fetch(url)
+//         .then((res) => res.json())
+//         .then((data) => { console.log(data) })
+// } catch (err) {
+//     console.log(err)
+// }
+
+try {
+    const fetchData = async (url) => {
+        const response = await fetch(url);
+        if (response.status !== 200) {
+            console.log("I'm not here")
+        } else {
+            console.log("I'm responding Don't worry!")
+        }
+        const data = await response.json();
+        console.log(data)
+    }
+    fetchData(url)
+} catch (err) {
+    console.log(err)
+}
+
+// await works like
--- week-4.js
+++ week-4.js
@@ -1,12 +1,12 @@
-// 1.Write a JavaScript program to check if a number is positive, negative, or zero. 
-// 2.Create a function that takes two numbers as arguments and returns their sum.  
-// 3.Write a program to find the maximum of two given numbers.  
-// 4.Create a function that takes a string as input and returns its length.  
-// 5.Write a JavaScript function to check if a given number is prime or not.  
-// 6.Write a function to sort given array.  
-// 7.Write a function that checks if a given string, is a palindrome.  
-// 8.Create a program that generates a random number between 1 and 10 and asks the user to guess it.  
-// 9. Create a program that generates a random password with a mixture of characters and symbols.  
+// 1.Write a JavaScript program to check if a number is positive, negative, or zero.
+// 2.Create a function that takes two numbers as arguments and returns their sum.
+// 3.Write a program to find the maximum of two given numbers.
+// 4.Create a function that takes a string as input and returns its length.
+// 5.Write a JavaScript function to check if a given number is prime or not.
+// 6.Write a function to sort given array.
+// 7.Write a function that checks if a given string, is a palindrome.
+// 8.Create a program that generates a random number between 1 and 10 and asks the user to guess it.
+// 9. Create a program that generates a random password with a mixture of characters and symbols.
 // 10.Create a program that determines if a given year is a leap year or not.
 
 // function checkNumberSign(num) {
@@ -21,7 +21,6 @@
 
 // const number = 10;
 // console.log(checkNumberSign(number)); // Output: "Positive"
-
 
 // function sumNumbers(a, b) {
 //     return a + b;
@@ -38,7 +37,6 @@
 // const num1 = 8;
 // const num2 = 12;
 // console.log(findMax(num1, num2)); // Output: 12
-
 
 // function getStringLength(str) {
 //     return str.length;
@@ -62,7 +60,6 @@
 // const number = 17;
 // console.log(isPrime(number)); // Output: true
 
-
 // function isPrime(num) {
 //     if (num <= 1) return false;
 
@@ -78,7 +75,6 @@
 // const number = 17;
 // console.log(isPrime(number)); // Output: true
 
-
 // function isPalindrome(str) {
 //     const reversedStr = str.split("").reverse().join("");
 //     return str === reversedStr;
@@ -86,7 +82,6 @@
 
 // const word = "racecar";
 // console.log(isPalindrome(word)); // Output: true
-
 
 // function generateRandomNumber() {
 //     return Math.floor(Math.random() * 10) + 1;
@@ -96,7 +91,6 @@
 // console.log("Guess a number between 1 and 10.");
 
 // // Implement the user input and check if it matches the generated random number.
-
 
 // function generateRandomPassword(length) {
 //     const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*";
@@ -113,10 +107,104 @@
 // const passwordLength = 10;
 // console.log(generateRandomPassword(passwordLength));
 
-
 // function isLeapYear(year) {
 //     return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
 // }
 
 // const year = 2024;
 // console.log(isLeapYear(year)); // Output: true
+
+// Given an array of integers, rearrange the elements in a way that all the even numbers
+// come before the odd numbers.
+
+function rearrangeEvenOdd(arr) {
+  return arr
+    .filter((num) => num % 2 === 0)
+    .concat(arr.filter((num) => num % 2 !== 0));
+}
+
+const numbers = [1, 2, 3, 4, 5, 6];
+const rearrangedArray = rearrangeEvenOdd(numbers);
+console.log(rearrangedArray);
+
+// Write a function to find the missing number in an array of integers from 1 to N.
+
+function findMissingNumber(arr, N) {
+  const expectedSum = (N * (N + 1)) / 2;
+  const actualSum = arr.reduce((sum, num) => sum + num, 0);
+  return expectedSum - actualSum;
+}
+
+const array = [1, 2, 4, 5, 6];
+const N = 6;
+const missingNumber = findMissingNumber(array, N);
+console.log(missingNumber); 
+
+// Given a string with parentheses, check if it is balanced (each opening parenthesis
+// has a corresponding closing parenthesis).
+
+// Implement a function to convert a decimal number to its binary representation.
+
+function decimalToBinary(decimalNum) {
+  return decimalNum.toString(2);
+}
+
+const decimalNumber = 13;
+const binaryRepresentation = decimalToBinary(decimalNumber);
+console.log(binaryRepresentation); 
+
+// Implement a function that returns the longest word in a sentence.
+function findLongestWord(sentence) {
+  const words = sentence.split(" ");
+  return words.reduce(
+    (longestWord, currentWord) =>
+      currentWord.length > longestWord.length ? currentWord : longestWord,
+    ""
+  );
+}
+
+const inputSentence = "The quick brown fox jumps over the lazy dog";
+const longestWord = findLongestWord(inputSentence);
+console.log(longestWord); 
+
+// Given a matrix (2D array), rotate it 90 degrees clockwise
+
+// Write a function to check if a given string contains only unique characters (no
+// repeating characters).
+function hasUniqueCharacters(str) {
+  const charSet = new Set();
+  for (const char of str) {
+    if (charSet.has(char)) {
+      return false;
+    }
+    charSet.add(char);
+  }
+  return true;
+}
+
+const uniqueStr = "abcdefg";
+const nonUniqueStr = "hello";
+console.log(hasUniqueCharacters(uniqueStr)); 
+console.log(hasUniqueCharacters(nonUniqueStr));
+
+// Given an array of integers and a target sum, find the index of two numbers that add
+// up to the target sum.
+
+// Write a function that generates all possible combinations of a given string.
+function generateStringCombinations(str) {
+  const result = [];
+  function backtrack(path, start) {
+    result.push(path);
+    for (let i = start; i < str.length; i++) {
+      backtrack(path + str[i], i + 1);
+    }
+  }
+  backtrack("", 0);
+  return result;
+}
+
+const inputString = "abc";
+const combinations = generateStringCombinations(inputString);
+console.log(combinations); // Output: [ 'abc', 'ab', 'ac', 'a', 'bc', 'b', 'c', '' ]
+
+// Given a string, find the most frequent character(s) and their count(s).
